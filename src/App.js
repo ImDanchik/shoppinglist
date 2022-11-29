252020859e237f69223dd42f1526439ccceb6640
Dan <Dan@Dans-MacBook-Pro.local>
2022-11-29 21:45:18 +0200

    more items

--- src/App.js
+++ src/App.js
@@ -18,7 +18,37 @@ const App = () => {
       image: "https://m.pricez.co.il/ProductPictures/7290008409100.jpg",
       title: "שניצל",
       text: "זה טעים",
+    },    {
+      image: "https://www.tnuva.co.il/uploads/f_606ee43fa87cf_1617880127.jpg",
+      title: "Milk",
+      text: "Whole 3% milk",
     },
+    {
+      image:
+        "https://osemcat.signature-it.com/images/Fittings/osem-hq/Upload_Pictures/Prod_Pic/6910091/Catalog/6910091_7290006567048_Enlarge.jpg",
+      title: " קוסקוס",
+      text: "זה כולה קוסקוס אחי",
+    },
+    {
+      image: "https://m.pricez.co.il/ProductPictures/7290008409100.jpg",
+      title: "שניצל",
+      text: "זה טעים",
+    },    {
+      image: "https://www.tnuva.co.il/uploads/f_606ee43fa87cf_1617880127.jpg",
+      title: "Milk",
+      text: "Whole 3% milk",
+    },
+    {
+      image:
+        "https://osemcat.signature-it.com/images/Fittings/osem-hq/Upload_Pictures/Prod_Pic/6910091/Catalog/6910091_7290006567048_Enlarge.jpg",
+      title: " קוסקוס",
+      text: "זה כולה קוסקוס אחי",
+    },
+    {
+      image: "https://m.pricez.co.il/ProductPictures/7290008409100.jpg",
+      title: "שניצל",
+      text: "זה טעים",
+    }
   ];
 
   return (
