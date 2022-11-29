361bce18051e87b5dc8f5027deefcd0092840a01
Dan <Dan@Dans-MacBook-Pro.local>
2022-11-29 21:55:38 +0200

    description

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import ShopItem from "./components/ShopItem";
-import { Row, Col } from "react-bootstrap";
 
 const App = () => {
   const items = [
@@ -7,55 +6,36 @@ const App = () => {
       image: "https://www.tnuva.co.il/uploads/f_606ee43fa87cf_1617880127.jpg",
       title: "Milk",
       text: "Whole 3% milk",
+      price: '5'
     },
     {
       image:
         "https://osemcat.signature-it.com/images/Fittings/osem-hq/Upload_Pictures/Prod_Pic/6910091/Catalog/6910091_7290006567048_Enlarge.jpg",
       title: " קוסקוס",
       text: "זה כולה קוסקוס אחי",
+      price: '10'
     },
     {
       image: "https://m.pricez.co.il/ProductPictures/7290008409100.jpg",
       title: "שניצל",
       text: "זה טעים",
-    },    {
-      image: "https://www.tnuva.co.il/uploads/f_606ee43fa87cf_1617880127.jpg",
-      title: "Milk",
-      text: "Whole 3% milk",
-    },
-    {
-      image:
-        "https://osemcat.signature-it.com/images/Fittings/osem-hq/Upload_Pictures/Prod_Pic/6910091/Catalog/6910091_7290006567048_Enlarge.jpg",
-      title: " קוסקוס",
-      text: "זה כולה קוסקוס אחי",
-    },
-    {
-      image: "https://m.pricez.co.il/ProductPictures/7290008409100.jpg",
-      title: "שניצל",
-      text: "זה טעים",
-    },    {
-      image: "https://www.tnuva.co.il/uploads/f_606ee43fa87cf_1617880127.jpg",
-      title: "Milk",
-      text: "Whole 3% milk",
-    },
-    {
-      image:
-        "https://osemcat.signature-it.com/images/Fittings/osem-hq/Upload_Pictures/Prod_Pic/6910091/Catalog/6910091_7290006567048_Enlarge.jpg",
-      title: " קוסקוס",
-      text: "זה כולה קוסקוס אחי",
-    },
-    {
-      image: "https://m.pricez.co.il/ProductPictures/7290008409100.jpg",
-      title: "שניצל",
-      text: "זה טעים",
+      price: '14'
     }
+
   ];
 
   return (
-    <div style={{ margin: `50px`, display: `flex`, flexDirection: `row`, justifyContent: `center` }}>
-      {
-        items.map(item => <ShopItem image={item.image} title={item.title} text={item.text} />)
-      }
+    <div
+      style={{
+        margin: `50px`,
+        display: `flex`,
+        flexDirection: `row`,
+        justifyContent: `center`,
+      }}
+    >
+      {items.map((item) => (
+        <ShopItem image={item.image} title={item.title} text={item.text} price={item.price}/>
+      ))}
     </div>
   );
 };
--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -1,15 +1,45 @@
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Modal } from "react-bootstrap";
+import React, { useState } from "react";
 
 const ShopItem = (props) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const changeShowModal = () => {
+    setShowModal(true);
+  };
+
   return (
-    <Card className="text-center">
-      <Card.Img style={{ width: "18vw" }} variant="top" src={props.image} />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text>{props.text}</Card.Text>
-        <Button variant="primary">Add to Cart</Button>
-      </Card.Body>
-    </Card>
+    <>
+      <Card className="text-center">
+        <Card.Img style={{ width: "18vw" }} variant="top" src={props.image} />
+        <Card.Body>
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text>₪ {props.price}</Card.Text>
+          <Button onClick={changeShowModal} variant="primary">
+            Description
+          </Button>
+        </Card.Body>
+      </Card>
+
+      <Modal show={showModal}>
+        <Modal.Header>
+          <Modal.Title>{props.title}</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>{props.text}</Modal.Body>
+
+        <Modal.Footer>
+          <button
+            className="btn btn-light"
+            onClick={() => {
+              setShowModal(false);
+            }}
+          >
+            Close
+          </button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
